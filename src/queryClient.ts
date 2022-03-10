5dabd25895745ec4368bd47e9a0c2d315080889b
tjscks0510 <tjscks0510@gmail.com>
2022-03-10 18:53:15 +0900

    - Third days, make component, update query [Description] - learn about graphQL query, mutation - make the component of cart item [Relevant] - https://tech.kakao.com/2019/08/01/graphql-basic/

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -1,30 +1,28 @@
-import { QueryClient } from 'react-query'
-import { request, RequestDocument } from 'graphql-request'
+import { QueryClient } from "react-query";
+import { request, RequestDocument } from "graphql-request";
 
-// import { getTodos, postTodo } from '../my-api'
-
-type AnyOBJ = { [key: string]: any }
+type AnyOBJ = { [key: string]: any };
 
 export const getClient = (() => {
-  let client: QueryClient | null = null
+  let client: QueryClient | null = null;
   return () => {
     if (!client)
       client = new QueryClient({
         defaultOptions: {
           queries: {
-            cacheTime: 1000 * 60 * 60 * 24,
-            staleTime: 1000 * 60,
+            staleTime: Infinity,
+            cacheTime: Infinity,
             refetchOnMount: false,
             refetchOnReconnect: false,
             refetchOnWindowFocus: false,
           },
         },
-      })
-    return client
-  }
-})()
+      });
+    return client;
+  };
+})();
 
-const BASE_URL = '/'
+const BASE_URL = "/";
 
 export const restFetcher = async ({
   method,
@@ -32,38 +30,38 @@ export const restFetcher = async ({
   body,
   params,
 }: {
-  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH'
-  path: string
-  body?: AnyOBJ
-  params?: AnyOBJ
+  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+  path: string;
+  body?: AnyOBJ;
+  params?: AnyOBJ;
 }) => {
   try {
-    let url = `${BASE_URL}${path}`
+    let url = `${BASE_URL}${path}`;
     const fetchOptions: RequestInit = {
       method,
       headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': BASE_URL,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": BASE_URL,
       },
-    }
+    };
     if (params) {
-      const searchParams = new URLSearchParams(params)
-      url += '?' + searchParams.toString()
+      const searchParams = new URLSearchParams(params);
+      url += "?" + searchParams.toString();
     }
-    if (body) fetchOptions.body = JSON.stringify(body)
+    if (body) fetchOptions.body = JSON.stringify(body);
 
-    const res = await fetch(url, fetchOptions)
-    const json = await res.json()
-    return json
+    const res = await fetch(url, fetchOptions);
+    const json = await res.json();
+    return json;
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 export const graphqlFetcher = (query: RequestDocument, variables = {}) =>
-  request(BASE_URL, query, variables)
+  request(BASE_URL, query, variables);
 
 export const QueryKeys = {
-  PRODUCTS: 'PRODUCTS',
-  CART: 'CART',
-}
+  PRODUCTS: "PRODUCTS",
+  CART: "CART",
+};
